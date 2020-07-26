d14f158605c462425ef0f7317eac4651ab443a25
SIVASHANKAR.S <sivarvs11@gmail.com>
2020-07-26 22:50:28 +0530

    PSET-8 webpage

---
+++ PSET-8/homepage/greet.js
@@ -0,0 +1,11 @@
+ function greet()
+            {
+              let name = document.querySelector('#name').value;
+              if (name ==='')
+              {
+                name='world';
+              }
+              alert('Hello, ' + name + '.!' )
+              alert('Thanks for visiting my HomePage 2')
+              alert('lets have fun...! Scroll Down')
+            }
